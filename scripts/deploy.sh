448038d1a86eb715c31792f2869b603e0abb1b60
Matthew Farwell <matthew@farwell.co.uk>
2013-06-21 09:31:14 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-(cd org.scalastyle.scalastyleplugin.update-site/target; /c/Program\ Files\ \(x86\)/PuTTY/pscp.exe -r -pw jPU7RPMu site.zip site  mfarwell_scalastyle@ssh.phx.nearlyfreespeech.net:downloads/helios/master)
+(cd org.scalastyle.scalastyleplugin.update-site/target; /c/Program\ Files\ \(x86\)/PuTTY/pscp.exe -r -pw xxx site.zip site  mfarwell_scalastyle@ssh.phx.nearlyfreespeech.net:downloads/helios/master)
